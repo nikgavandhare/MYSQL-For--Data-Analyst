207292c26c95d0b5027c8aa5092861f21566ec25
nikgavandhare <gaondharenikita@gmail.com>
2025-02-12 12:58:57 +0530

    initial commit

---
+++ 2.SELECT_Tutorial.sql
@@ -0,0 +1,15 @@
+# -----------------  SELECT  QUERY ---------------------
+
+
+SELECT * FROM parks_and_recreation.employee_demographics;
+
+Select first_name,
+last_name,
+age,
+age+10 from parks_and_recreation.employee_demographics;
+# age +10 col added 
+
+
+select distinct first_name,gender
+from parks_and_recreation.employee_demographics;
+
---
+++ 3.WHERE_CLAUSE_Tutorial.sql
@@ -0,0 +1,41 @@
+# --------------------- WHERE CLAUSE ------------------
+
+
+#  employee_salary  Table
+SELECT * FROM employee_salary;
+
+#1-->
+
+SELECT *
+FROM employee_salary
+WHERE dept_id = 3;
+
+
+#2 -->
+
+select * 
+from employee_salary
+where salary>50000 ;
+
+
+#  employee_demographics Table
+select * from employee_demographics;
+
+#1-->
+
+select * 
+from employee_demographics
+where gender !='female';
+
+#2-->
+
+select * 
+from employee_demographics
+where birth_date > '1984-01-01'  OR gender='male';
+
+
+#3-->
+
+select * 	
+from employee_demographics
+where age>40 AND gender='female';
