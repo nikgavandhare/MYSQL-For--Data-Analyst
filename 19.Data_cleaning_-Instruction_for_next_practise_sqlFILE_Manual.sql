afe3ee7fd4eb017ca9664cd57ddfc1059286ac21
nikgavandhare <gaondharenikita@gmail.com>
2025-03-25 16:48:39 +0530

    added

--- 19.Data_cleaning_-Instruction_for_next_practise_sqlFILE_Manual.sql
+++ 19.Data_cleaning_-Instruction_for_next_practise_sqlFILE_Manual.sql
@@ -2,7 +2,7 @@
 
 --   Data Cleaning  --
 
--- here we are using  anothe dat base called world_layoffs
+-- here we are using  anothe database called:-  world_layoffs    ( we get this dataset from website- "https://www.kaggle.com/datasets/swaptr/layoffs-2022"  )
 -- then in world_layoffs --> table--> right click -->table data import --> slected file layoff.cvs file form mysql folder 
 -- the imported data 
 
